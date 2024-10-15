68803b745c453ab546357d2f83b7b6ed55f3097c
Wilson Liang <wilson.l20010327@gmail.com>
2024-10-15 19:11:40 -0400

    Add try catch to handle the connect error
    Add try catch to grab the error when the error happend in the
    connection.

--- src/DatabaseManager.cpp
+++ src/DatabaseManager.cpp
@@ -19,60 +19,84 @@ bsoncxx::document::value DatabaseManager::createDocument(
 }
 
 void DatabaseManager::createCollection(const std::string& collectionName) {
-  conn["GitGud"][collectionName];  // Create collection if it doesn't exist
+  try {
+    conn["GitGud"][collectionName];  // Create collection if it doesn't exist
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n';
+  }
 }
 
 void DatabaseManager::printCollection(const std::string& collectionName) {
-  auto collection = conn["GitGud"][collectionName];
-  if (collection.count_documents({}) == 0) {
-    std::cout << "Collection " << collectionName << " is empty." << std::endl;
-    return;
-  }
+  try {
+    auto collection = conn["GitGud"][collectionName];
+    if (collection.count_documents({}) == 0) {
+      std::cout << "Collection " << collectionName << " is empty." << std::endl;
+      return;
+    }
 
-  auto cursor = collection.find({});
-  for (auto&& doc : cursor) {
-    std::cout << bsoncxx::to_json(doc) << std::endl;
+    auto cursor = collection.find({});
+    for (auto&& doc : cursor) {
+      std::cout << bsoncxx::to_json(doc) << std::endl;
+    }
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n';
   }
 }
 
 void DatabaseManager::insertResource(
     const std::string& collectionName,
     const std::vector<std::pair<std::string, std::string>>& keyValues) {
-  auto collection = conn["GitGud"][collectionName];
-  collection.insert_one(createDocument(keyValues).view());
+  try {
+    auto collection = conn["GitGud"][collectionName];
+    collection.insert_one(createDocument(keyValues).view());
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n' << "error";
+  }
 }
 
 void DatabaseManager::deleteResource(const std::string& collectionName,
                                      const std::string& resourceId) {
-  auto collection = conn["GitGud"][collectionName];
-  collection.delete_one(createDocument({{"_id", resourceId}}).view());
+  try {
+    auto collection = conn["GitGud"][collectionName];
+    collection.delete_one(createDocument({{"_id", resourceId}}).view());
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n' << "error";
+  }
 }
 
 void DatabaseManager::updateResource(
     const std::string& collectionName, const std::string& resourceId,
     const std::vector<std::pair<std::string, std::string>>& updates) {
-  auto collection = conn["GitGud"][collectionName];
-  bsoncxx::builder::stream::document updateDoc{};
-  updateDoc << "$set" << bsoncxx::builder::stream::open_document;
-  for (const auto& update : updates) {
-    updateDoc << update.first << update.second;
-  }
-  updateDoc << bsoncxx::builder::stream::close_document;
+  try {
+    auto collection = conn["GitGud"][collectionName];
+    bsoncxx::builder::stream::document updateDoc{};
+    updateDoc << "$set" << bsoncxx::builder::stream::open_document;
+    for (const auto& update : updates) {
+      updateDoc << update.first << update.second;
+    }
+    updateDoc << bsoncxx::builder::stream::close_document;
 
-  collection.update_one(bsoncxx::builder::stream::document{}
-                            << "_id" << resourceId
-                            << bsoncxx::builder::stream::finalize,
-                        updateDoc.view());
+    collection.update_one(bsoncxx::builder::stream::document{}
+                              << "_id" << resourceId
+                              << bsoncxx::builder::stream::finalize,
+                          updateDoc.view());
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n' << "error";
+  }
 }
 
 void DatabaseManager::findResource(const std::string& collectionName,
                                    const std::string& resourceId) {
-  auto collection = conn["GitGud"][collectionName];
-  auto filter = bsoncxx::builder::stream::document{}
-                << "_id" << resourceId << bsoncxx::builder::stream::finalize;
-  auto cursor = collection.find(filter.view());
-  for (auto&& doc : cursor) {
-    std::cout << bsoncxx::to_json(doc) << std::endl;
+  try {
+    auto collection = conn["GitGud"][collectionName];
+    auto filter = bsoncxx::builder::stream::document{}
+                  << "_id" << resourceId << bsoncxx::builder::stream::finalize;
+    auto cursor = collection.find(filter.view());
+    for (auto&& doc : cursor) {
+      std::cout << bsoncxx::to_json(doc) << std::endl;
+    }
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n' << "error";
   }
 }
 
