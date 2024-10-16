d9612a061c9118aed3765885b1468328de8ead2d
Brendon <b10815044@gapps.ntust.edut.tw>
2024-10-16 13:49:00 -0400

    implement Counseling router

--- include/RouteController.h
+++ include/RouteController.h
@@ -12,25 +12,35 @@
 #include "../external_libraries/Crow/include/crow.h"
 #include "DatabaseManager.h"
 #include "Shelter.h"
+#include "Counseling.h"
+
 class RouteController {
  private:
   DatabaseManager& dbManager;  // Reference to the database manager
 
  public:
-  // Add a constructor
+  // Constructor
   RouteController(DatabaseManager& dbManager) : dbManager(dbManager) {}
 
-  // Add the initRoutes method declaration
+  // Initialize routes
   void initRoutes(crow::SimpleApp& app);
 
   // Route handlers
   void index(crow::response& res);
   void getResources(const crow::request& req, crow::response& res);
   void addResource(const crow::request& req, crow::response& res);
-  void addShelter(const crow::request& req, crow::response& res);
-  void getShelter(const crow::request& req, crow::response& res);
   void updateResource(const crow::request& req, crow::response& res);
   void deleteResource(const crow::request& req, crow::response& res);
+  
+  // Shelter-related handlers
+  void addShelter(const crow::request& req, crow::response& res);
+  void getShelter(const crow::request& req, crow::response& res);
+  
+  // Counseling-related handlers
+  void getCounseling(const crow::request& req, crow::response& res);
+  void addCounseling(const crow::request& req, crow::response& res);
+  void updateCounseling(const crow::request& req, crow::response& res);
+  void deleteCounseling(const crow::request& req, crow::response& res);
 };
 
 #endif
