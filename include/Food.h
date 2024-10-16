c7c8255ef9612c6b2bad6bb9b6819d7b451cfb6f
david-chenyu <davidb891128@gmail.com>
2024-10-16 01:29:07 -0400

    remove commented code

--- include/Food.h
+++ include/Food.h
@@ -1,40 +1,20 @@
-// #ifndef FOOD_H
-// #define FOOD_H
-
-// #include <string>
-
-// class Food {
-// public:
-//     Food(const std::string& type, const std::string& quantity);
-    
-//     std::string getType() const;
-//     std::string getQuantity() const;
-
-// private:
-//     std::string type;
-//     std::string quantity;
-// };
-
-// #endif 
 #ifndef FOOD_RESOURCE_H
 #define FOOD_RESOURCE_H
 
 #include <vector>
 #include <string>
-#include <utility> // for std::pair
-#include "DatabaseManager.h"  // Ensure dbManager is included
+#include <utility> 
+#include "DatabaseManager.h"  
 
 class Food {
 private:
-    std::vector<std::pair<std::string, std::string>> resource;  // Data for the food resource
+    std::vector<std::pair<std::string, std::string>> resource;  
     DatabaseManager& db;  // Reference to the database manager
 
 public:
-    // Constructor to initialize Food with keyValues and dbManager
     Food(const std::vector<std::pair<std::string, std::string>>& reasource, DatabaseManager& db);
 
-    // Method to insert food into the database
     void insertFood();
 };
 
-#endif // FOOD_RESOURCE_H
+#endif 
