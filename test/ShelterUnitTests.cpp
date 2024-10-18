8b0b983cce8f377e9734a6ac06eaf3e7b7524dd0
Wilson Liang <wilson.l20010327@gmail.com>
2024-10-18 11:40:06 -0400

    Create the Shelter unit test
    Because the EXPECT_CALL will cause segmentation fault in Ubuntu and
    cannot find a way to go thought it, I change into use ON_CALL. Besides,
    I add some description in shelter.

--- test/ShelterUnitTests.cpp
+++ test/ShelterUnitTests.cpp
@@ -2,6 +2,9 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/json.hpp>
+
 #include "Shelter.h"
 class MockDatabaseManager : public DatabaseManager {
  public:
@@ -37,37 +40,34 @@ class ShelterUnitTests : public ::testing::Test {
 TEST_F(ShelterUnitTests, searchShelterAll) {
   std::vector<bsoncxx::document::value> mockResult;
   mockResult.push_back(bsoncxx::builder::stream::document{}
-                       << "ORG" << "tmp"
-                       << "User" << "tmp"
-                       << "location" << "tmp"
-                       << "capacity" << "1"
+                       << "ORG" << "NGO"
+                       << "User" << "HML"
+                       << "location" << "New York"
+                       << "capacity" << "10"
                        << "curUse" << "0"
                        << bsoncxx::builder::stream::finalize);
 
-  EXPECT_CALL(*mockDbManager,
-              findCollection(::testing::_, ::testing::_, ::testing::_))
-      .WillOnce(::testing::DoAll(::testing::SetArgReferee<2>(mockResult),
-                                 ::testing::Return()));
+  ON_CALL(*mockDbManager,
+          findCollection(::testing::_, ::testing::_, ::testing::_))
+      .WillByDefault(::testing::DoAll(::testing::SetArgReferee<2>(mockResult),
+                                      ::testing::Return()));
 
   std::string services = shelter->searchShelterAll();
-
   EXPECT_NE(services, "[]");
-  EXPECT_TRUE(services.find("tmp") != std::string::npos);
-  EXPECT_TRUE(services.find("General Checkup") != std::string::npos);
-  EXPECT_TRUE(services.find("tmp") != std::string::npos);
+  EXPECT_TRUE(services.find("NGO") != std::string::npos);
+  EXPECT_TRUE(services.find("HML") != std::string::npos);
+  EXPECT_TRUE(services.find("New York") != std::string::npos);
 }
 TEST_F(ShelterUnitTests, AddNewShelter) {
   std::vector<std::pair<std::string, std::string>> expectedContent =
       shelter->createDBContent("tmp", "tmp", "tmp", "1", "0");
-
-  EXPECT_CALL(*mockDbManager, insertResource(::testing::_, ::testing::_))
-      .WillOnce(::testing::Invoke(
+  ON_CALL(*mockDbManager, insertResource(::testing::_, ::testing::_))
+      .WillByDefault(
           [&](const std::string& collectionName,
               const std::vector<std::pair<std::string, std::string>>& content) {
             EXPECT_EQ(collectionName, "ShelterTest");
             EXPECT_EQ(content, expectedContent);
-          }));
-
+          });
   std::string ret = shelter->addShelter("tmp", "tmp", "tmp", 1, 0);
   EXPECT_EQ(ret, "Success");
 }
