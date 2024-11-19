e766730c543a301b935d82ff197f0b4a6db45f2f
suranimaria <surani_maria@yahoo.com>
2024-11-19 17:26:01 -0500

    Add Poco and dependencies installation steps

--- setup.sh
+++ setup.sh
@@ -15,6 +15,12 @@ COVERAGE_DIR="$BUILD_DIR/coverage"
 # Create external_libraries directory if it doesn't exist
 mkdir -p "$EXTERNAL_LIBRARIES_DIR"
 
+# Install Poco dependencies
+echo "Installing Poco dependencies..."
+sudo apt-get update
+sudo apt-get install -y openssl libssl-dev
+
+
 # Install external libraries
 echo "Starting the download of external libraries..."
 
@@ -50,6 +56,21 @@ if [ ! -d "mongo-cxx-driver" ]; then
     cd ../..
 else
     echo "MongoDB C++ Driver already exists, skipping installation."
+fi
+
+# Download and install Poco library
+if [ ! -d "Poco" ]; then
+    echo "Downloading and installing Poco library..."
+    git clone https://github.com/pocoproject/poco.git Poco
+    cd Poco || { echo "Failed to change to Poco directory"; exit 1; }
+    mkdir -p cmake-build
+    cd cmake-build || { echo "Failed to change to Poco build directory"; exit 1; }
+    cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$EXTERNAL_LIBRARIES_DIR/Poco"
+    make -j$(nproc)
+    make install
+    cd ../../
+else
+    echo "Poco library already exists, skipping download and installation."
 fi
 
 echo "All external libraries processed successfully."
