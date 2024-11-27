0ac4b37d2c881a557e892cd0209da6a0aab57826
suranimaria <surani_maria@yahoo.com>
2024-11-26 23:08:41 -0500

    Add database tests

--- test/DataBaseTest.cpp
+++ test/DataBaseTest.cpp
@@ -29,3 +29,68 @@ TEST_F(DataBaseTest, Checksetup) {
   DbManager->findCollection(0, "test", {}, result);
   EXPECT_EQ(result.size(), 0);
 }
+
+TEST_F(DataBaseTest, InsertResourceTest) {
+  std::string id = DbManager->insertResource("test", {{"Name", "Resource A"}, {"Type", "Test"}});
+  EXPECT_FALSE(id.empty());
+
+  std::vector<bsoncxx::document::value> result;
+  DbManager->findCollection(0, "test", {}, result);
+  EXPECT_EQ(result.size(), 1);
+
+  auto doc = bsoncxx::to_json(result[0].view());
+  EXPECT_NE(doc.find("Resource A"), std::string::npos);
+  EXPECT_NE(doc.find("Test"), std::string::npos);
+}
+
+TEST_F(DataBaseTest, DeleteResourceTest) {
+  std::string id = DbManager->insertResource("test", {{"Name", "Resource B"}, {"Type", "Test"}});
+  EXPECT_FALSE(id.empty());
+
+  bool success = DbManager->deleteResource("test", id);
+  EXPECT_TRUE(success);
+
+  std::vector<bsoncxx::document::value> result;
+  DbManager->findCollection(0, "test", {}, result);
+  EXPECT_EQ(result.size(), 0);
+}
+
+TEST_F(DataBaseTest, UpdateResourceTest) {
+  std::string id = DbManager->insertResource("test", {{"Name", "Resource C"}, {"Type", "OldType"}});
+  EXPECT_FALSE(id.empty());
+
+  DbManager->updateResource("test", id, {{"Type", "NewType"}});
+
+  std::vector<bsoncxx::document::value> result;
+  DbManager->findCollection(0, "test", {}, result);
+  ASSERT_EQ(result.size(), 1);
+
+  auto doc = bsoncxx::to_json(result[0].view());
+  EXPECT_NE(doc.find("Resource C"), std::string::npos);
+  EXPECT_NE(doc.find("NewType"), std::string::npos);
+}
+
+TEST_F(DataBaseTest, PrintCollectionTest) {
+  DbManager->insertResource("test", {{"Name", "Resource E"}, {"Type", "Test"}});
+  DbManager->insertResource("test", {{"Name", "Resource F"}, {"Type", "Test"}});
+
+  testing::internal::CaptureStdout();
+  DbManager->printCollection("test");
+  std::string output = testing::internal::GetCapturedStdout();
+
+  EXPECT_NE(output.find("Resource E"), std::string::npos);
+  EXPECT_NE(output.find("Resource F"), std::string::npos);
+}
+
+TEST_F(DataBaseTest, GetResourcesTest) {
+  DbManager->insertResource("Resources", {{"type", "Food"}, {"Name", "Resource G"}});
+  DbManager->insertResource("Resources", {{"type", "Food"}, {"Name", "Resource H"}});
+  DbManager->insertResource("Resources", {{"type", "Shelter"}, {"Name", "Resource I"}});
+
+  auto resources = DbManager->getResources("Food");
+  auto doc = bsoncxx::to_json(resources.view());
+
+  EXPECT_NE(doc.find("Resource G"), std::string::npos);
+  EXPECT_NE(doc.find("Resource H"), std::string::npos);
+  EXPECT_EQ(doc.find("Resource I"), std::string::npos); 
+}
